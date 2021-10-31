cc668977dbf48af73f9cf1bc755050f0b3281d59
nheingit <60185486+nheingit@users.noreply.github.com>
2021-10-31 10:00:41 -0500

    leaked env var and rolled bot token

--- deploy-commands.ts
+++ deploy-commands.ts
@@ -2,7 +2,10 @@
 import { SlashCommandBuilder } from '@discordjs/builders';
 import { REST } from '@discordjs/rest';
 import { Routes } from 'discord-api-types/v9';
+import dotenv from 'dotenv'
 import fs from 'fs';
+
+dotenv.config()
 
 const commands: SlashCommandBuilder[] = []
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.ts'));
@@ -11,10 +14,11 @@ const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('
             const command = await import(`./commands/${file}`);
             commands.push(command.data.toJSON())
         }
-        const rest = new REST({ version: '9' }).setToken('OTAzNDk2MzQwMzUxMzczMzUy.YXt0iQ.jYWVsZ1r_-ajmlaBa-0eAeabpXQ')
+        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+        const rest = new REST({ version: '9' }).setToken(process!.env.DISCORD_TOKEN!)
         try {
             rest.put(Routes.applicationGuildCommands('903496340351373352', '904099881323487253'), { body: commands })
-                .then(()=> console.log('registered / commands'))
+                .then(() => console.log('registered / commands'))
                 .catch(console.error)
         }
         catch { console.error }
