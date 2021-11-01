ddd056d92adac288c933fe7640969f9b0609de38
nheingit <60185486+nheingit@users.noreply.github.com>
2021-10-31 23:25:15 -0500

    added collection functionality

--- commands/addResource.ts
+++ commands/addResource.ts
@@ -1,5 +1,5 @@
 import { SlashCommandBuilder } from '@discordjs/builders';
-import { CommandInteraction } from 'discord.js';
+import { CommandInteraction, Message } from 'discord.js';
 import isValidUrl from '../utils/urlChecker';
 
 export const data = new SlashCommandBuilder()
@@ -12,11 +12,36 @@ export const data = new SlashCommandBuilder()
 
 export async function execute(interaction: CommandInteraction) {
     const userInput = interaction.options.getString('input')
+    const validAnswers = ['y', 'n']
+    const filter = (response: Message) => {
+        return validAnswers.some(answer => answer.toLowerCase() === response.content.toLowerCase()) && response.author.id === interaction.user.id
+    }
+
     // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
     if (isValidUrl(userInput!)) {
-        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-        console.log(interaction.channel)
-        await interaction.user.send(`this is the link you sent: ${userInput} is this the right link?`)
+        await interaction.reply({
+            content: `you are submiting ${userInput} as a new resource. \n Is this link correct? [Y/N]`,
+            ephemeral: true,
+        })
+        const collection = await interaction.channel?.awaitMessages({ filter, max: 1, time: 15000, errors: ['time'] })
+        collection?.map((message: Message) => {
+            switch (message.content.toLowerCase()) {
+                case 'y':
+                    interaction.followUp({
+                        content: 'Confirmed!',
+                        ephemeral: true,
+                    })
+                    break;
+                case 'n':
+                    interaction.followUp({
+                        content: 'Try again with the correct resource please!',
+                        ephemeral: true,
+                    })
+                    break;
+                default:
+                    break;
+            }
+        })
     }
     else {
         await interaction.reply({ content: 'Sorry, that\'s not a valid url!', ephemeral: true })
--- index.ts
+++ index.ts
@@ -7,7 +7,6 @@ dotenv.config()
 const botToken = process.env.DISCORD_TOKEN
 const client = new discordClient({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 
-
 client.commands = new Collection();
 client.loadCommandsToClient();
 client.loadEventsToClient();
