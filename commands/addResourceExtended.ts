13fff43e14ddab95a32a4da18ec9fd9fd31a2f8e
Aiden Montgomery <aiden@constructivecoding.com>
2021-11-02 12:55:51 +0000

    Changed display of captured information into an embed

--- commands/addResourceExtended.ts
+++ commands/addResourceExtended.ts
@@ -1,5 +1,5 @@
-import { SlashCommandBuilder } from '@discordjs/builders';
-import { CommandInteraction, Message } from 'discord.js';
+import { Embed, SlashCommandBuilder } from '@discordjs/builders';
+import { CommandInteraction, DMChannel, Message, MessageEmbed } from 'discord.js';
 import isValidUrl from '../utils/urlChecker';
 
 import { setCategorySelection, setBlockchainSelection, setLevelSelection, setMediaTypeSelection, setTagSelection } from './menuSelections';
@@ -11,7 +11,28 @@ export const data = new SlashCommandBuilder()
     .addStringOption(
         option => option.setRequired(true)
         .setName('url')
-        .setDescription('Enter a link to a resource'))
+        .setDescription('Enter a link to a resource'));
+
+function ConfigureEmbed(embed: MessageEmbed, resource: Resource): MessageEmbed {
+  embed.setAuthor(resource.author ?? 'Author');
+  embed.setTitle(resource.title ?? 'Title');
+  embed.setURL(resource.source ?? 'Source');
+  embed.setDescription(resource.summary ?? 'Summary');
+  embed.setFields([
+    { name: 'level', value: resource.level ?? 'Level', inline: true },
+    { name: 'mediatype', value: resource.mediaType ?? 'Media Type', inline: true },
+    { name: 'blockchain', value: resource.blockchain ? resource.blockchain.join(', ') : 'Blockchain', inline: false },
+    { name: 'category', value: resource.category ? resource.category.join(', ') : 'Category', inline: true },
+    { name: 'tags', value: resource.tags ? resource.tags.join(', ') : 'Tags', inline: true },
+  ]);
+
+  return embed;
+}
+
+function UpdateEmbed(embed: MessageEmbed, embedMessage: Message, resource: Resource): void {
+  const updatedEmbed = ConfigureEmbed(new MessageEmbed(embed), resource);
+  embedMessage.edit({ embeds: [updatedEmbed] })
+}
 
 export async function execute(interaction: CommandInteraction) {
     const userInput = interaction.options.getString('url')
@@ -27,15 +48,19 @@ export async function execute(interaction: CommandInteraction) {
         const filter = (m: Message) => interaction.user.id === m.author.id;
         await interaction.reply('Please complete the addition process in the DM you just received');
         const dmChannel = await interaction.user.createDM();
-        await dmChannel.sendTyping();
-        await dmChannel.send('Please enter more information about the article');
-        const originalMessage = await dmChannel.send('Article title');
 
+        await dmChannel.send('Please enter more information about the article');
+
+        const resourceEmbed = ConfigureEmbed(new MessageEmbed(), newResource);
+        const embedMessage = await dmChannel.send({ embeds: [resourceEmbed] });
+        const originalMessage = await dmChannel.send('Article title');
         const receivedMessages = await originalMessage.channel.awaitMessages({ filter, max: 1 })
         const titleMessage = receivedMessages.first();
         if (titleMessage) {
           newResource.title = titleMessage.content ?? ''
-          originalMessage.edit(`Title: ${newResource.title}`);
+          // originalMessage.edit(`Title: ${newResource.title}`);
+          originalMessage.delete();
+          UpdateEmbed(resourceEmbed, embedMessage, newResource);
         }
 
         const summaryRequest = await dmChannel.send('Article summary');
@@ -43,34 +68,46 @@ export async function execute(interaction: CommandInteraction) {
         const summaryMessage = summaryMessages.first();
         if (summaryMessage) {
           newResource.summary = summaryMessage.content ?? ''
-          summaryRequest.edit(`Summary: ${newResource.summary}`);
+          // summaryRequest.edit(`Summary: ${newResource.summary}`);
+          summaryRequest.delete();
+          UpdateEmbed(resourceEmbed, embedMessage, newResource);
         }
 
         const levelMessage = await setLevelSelection(dmChannel);
         const levelResponse = await dmChannel.awaitMessageComponent<'SELECT_MENU'>()
         newResource.level = levelResponse.values[0];
-        levelMessage.edit({ content: `Level: ${newResource.level}`, components: [] });
+        // levelMessage.edit({ content: `Level: ${newResource.level}`, components: [] });
+        levelMessage.delete();
         newResource.level = levelResponse.values[0];
+        UpdateEmbed(resourceEmbed, embedMessage, newResource);
 
         const mediaMessage = await setMediaTypeSelection(dmChannel);
         const mediaTypeResponse = await dmChannel.awaitMessageComponent<'SELECT_MENU'>()
         newResource.mediaType = mediaTypeResponse.values[0];
-        mediaMessage.edit({ content: `Media: ${newResource.mediaType}`, components: [] })
+        // mediaMessage.edit({ content: `Media: ${newResource.mediaType}`, components: [] })
+        mediaMessage.delete();
+        UpdateEmbed(resourceEmbed, embedMessage, newResource);
 
         const blockchainMessage = await setBlockchainSelection(dmChannel);
         const blockchainResponse = await dmChannel.awaitMessageComponent<'SELECT_MENU'>()
         newResource.blockchain = blockchainResponse.values;
-        blockchainMessage.edit({ content: `Blockchain: ${newResource.blockchain.join(', ')}`, components: [] })
+        // blockchainMessage.edit({ content: `Blockchain: ${newResource.blockchain.join(', ')}`, components: [] })
+        blockchainMessage.delete();
+        UpdateEmbed(resourceEmbed, embedMessage, newResource);
 
         const tagsMessage = await setTagSelection(dmChannel);
         const tagsResponse = await dmChannel.awaitMessageComponent<'SELECT_MENU'>()
         newResource.tags = tagsResponse.values;
-        tagsMessage.edit({ content: `Tags: ${newResource.tags.join(', ')}`, components: [] })
+        // tagsMessage.edit({ content: `Tags: ${newResource.tags.join(', ')}`, components: [] })
+        tagsMessage.delete();
+        UpdateEmbed(resourceEmbed, embedMessage, newResource);
 
         const categoryMessage = await setCategorySelection(dmChannel);
         const categoryResponse = await dmChannel.awaitMessageComponent<'SELECT_MENU'>()
         newResource.category = categoryResponse.values;
-        categoryMessage.edit({ content: `Categories: ${newResource.category.join(', ')}`, components: [] })
+        // categoryMessage.edit({ content: `Categories: ${newResource.category.join(', ')}`, components: [] })
+        categoryMessage.delete();
+        UpdateEmbed(resourceEmbed, embedMessage, newResource);
 
         console.log(newResource)
     }
