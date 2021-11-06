251ef542fecae8a62ee28225af624320357358b7
Camila Rondinini <crondinini@gmail.com>
2021-11-06 20:29:34 +0000

    review(@with-heart): add function doc

--- utils/normalizeString.ts
+++ utils/normalizeString.ts
@@ -1,4 +1,10 @@
-function titleize(value:string) {
+/**
+ * titleize makes the first letter of every word uppercase and the rest lowercase.
+ * i.e.:
+ * titleize("jAnE dOe") => "Jane Doe"
+ * titleize("jane doe") => "Jane Doe"
+ */
+function titleize(value: string) {
   return value.toLowerCase().replace(/(?:^|\s|-)\S/g, x => x.toUpperCase())
 }
 
