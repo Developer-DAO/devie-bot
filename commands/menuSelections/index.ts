21281982b6f73ba7d7f95a9230a313622caff483
Aiden Montgomery <aiden@constructivecoding.com>
2021-11-14 14:50:35 +0000

    Added documentation for Heroku

--- commands/menuSelections/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './authorSelection';
-export * from './blockchainSelection';
-export * from './categorySelection';
-export * from './levelSelection';
-export * from './mediaTypeSelection';
-export * from './tagSelection';
