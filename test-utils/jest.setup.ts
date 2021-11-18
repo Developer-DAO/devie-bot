8ae61ed57a311d6f0da26392b3cdc4ff577f672d
Camila Rondinini <crondinini@gmail.com>
2021-11-18 19:20:00 +0100

    fix: add fake process env for testing

--- test-utils/jest.setup.ts
+++ test-utils/jest.setup.ts
@@ -1,7 +1,11 @@
 import { InternalDiscordManager } from './setup';
 import { server } from './mock-server';
 
-process.env.DISCORD_BOT_TOKEN = 'FAKE_BOT_TOKEN'
+process.env.DISCORD_BOT_TOKEN = 'FAKE_BOT_TOKEN';
+process.env.AIRTABLE_TOKEN = 'FAKE_AIRTABLE_TOKEN';
+process.env.AIRTABLE_BASE = 'FAKE_TABLE_NAME';
+process.env.GUILD_ID = 'DEV_DAO_ID';
+process.env.CLIENT_ID = 'FAKE_CLIENT_ID';
 
 beforeEach(() => jest.spyOn(Date, 'now'))
 beforeAll(() => server.listen({ onUnhandledRequest: 'error' }))
