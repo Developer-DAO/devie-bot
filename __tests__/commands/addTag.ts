8353854ff2b469be1aa87bcda60070cd98aaf5fc
Camila Rondinini <crondinini@gmail.com>
2021-11-18 18:54:04 +0100

    fix: path for test and add test to workflow

--- __tests__/commands/addTag.ts
+++ __tests__/commands/addTag.ts
@@ -1,14 +1,14 @@
-import { ApplicationCommandOptionTypes } from 'discord.js/typings/enums';
-import { execute } from '../../commands/addTag';
-import { setup } from '../../test-utils/setup';
-import { server } from '../../test-utils/test-server';
+import { ApplicationCommandOptionTypes } from "discord.js/typings/enums";
+import { execute } from "../../commands/addTag";
+import { setup } from "../../test-utils/setup";
+import { server } from '../../test-utils/mock-server';
 
 describe('Command add-tag', () => {
   it('should not run if tag is null', async () => {
     const { createCommandInteraction } = await setup();
     const interaction = createCommandInteraction(
       'add-tag',
-      [{ name: 'tag', type: ApplicationCommandOptionTypes.STRING, value: null }],
+      [{ name: 'tag', type: ApplicationCommandOptionTypes.STRING, value: null }]
     );
 
     // Instead of mocking this function, we could (should?)
