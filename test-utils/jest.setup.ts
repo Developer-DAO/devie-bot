7512aa1ab8feec543c251881c5e70ddbd4ff322c
Camila Rondinini <crondinini@gmail.com>
2021-11-19 16:43:57 +0100

    review(@AidenMontgomery): correct env var name

--- test-utils/jest.setup.ts
+++ test-utils/jest.setup.ts
@@ -1,7 +1,7 @@
 import { InternalDiscordManager } from './setup';
 import { server } from './mock-server';
 
-process.env.DISCORD_BOT_TOKEN = 'FAKE_BOT_TOKEN';
+process.env.DISCORD_TOKEN = 'FAKE_BOT_TOKEN';
 process.env.AIRTABLE_TOKEN = 'FAKE_AIRTABLE_TOKEN';
 process.env.AIRTABLE_BASE = 'FAKE_TABLE_NAME';
 process.env.GUILD_ID = 'DEV_DAO_ID';
