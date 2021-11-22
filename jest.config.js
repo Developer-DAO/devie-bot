013cc863df174afd836fab745be87ad7027de796
Camila Rondinini <crondinini@gmail.com>
2021-11-23 00:14:22 +0100

    review(@AidenMontgomery): import envs correctly

--- jest.config.js
+++ jest.config.js
@@ -3,5 +3,6 @@ module.exports = {
   modulePathIgnorePatterns: ['<rootDir>/dist/'],
   preset: 'ts-jest',
   testEnvironment: 'node',
+  setupFiles: ['<rootDir>/test-utils/jest.env.ts'],
   setupFilesAfterEnv: ['<rootDir>/test-utils/jest.setup.ts'],
 };
--- test-utils/jest.setup.ts
+++ test-utils/jest.setup.ts
@@ -1,12 +1,6 @@
 import { InternalDiscordManager } from './setup';
 import { server } from './mock-server';
 
-process.env.DISCORD_TOKEN = 'FAKE_BOT_TOKEN';
-process.env.AIRTABLE_TOKEN = 'FAKE_AIRTABLE_TOKEN';
-process.env.AIRTABLE_BASE = 'FAKE_TABLE_NAME';
-process.env.GUILD_ID = 'DEV_DAO_ID';
-process.env.CLIENT_ID = 'FAKE_CLIENT_ID';
-
 beforeEach(() => jest.spyOn(Date, 'now'))
 beforeAll(() => server.listen({ onUnhandledRequest: 'error' }))
 afterAll(() => server.close());
